b23e70783a3785155323ba326b7e06fe9945195f
Rayh <railande.dasilva@gmail.com>
2023-09-27 21:38:28 -0300

    coming soon page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,14 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      CriasLtda
+      <div className="hero min-h-screen" style={{backgroundImage: 'url(https://png.pngtree.com/background/20230606/original/pngtree-brightly-colored-tree-with-water-reflecting-on-the-top-in-a-picture-image_2881670.jpg)'}}>
+        <div className="hero-overlay bg-opacity-65"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Crias Ltda</h1>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
--- src/app/rayh/page.tsx
+++ src/app/rayh/page.tsx
@@ -5,7 +5,7 @@ export default function RayhHome(){
     return(
         <main>
             <div className="hero min-h-screen" style={{backgroundImage: 'url(https://images2.alphacoders.com/132/1329016.png)'}}>
-            <div className="hero-overlay bg-opacity-65"></div>
+                <div className="hero-overlay bg-opacity-65"></div>
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-md">
                         <h1 className="mb-5 text-5xl font-bold">Rayh</h1>
