e9082c30cf7ee5e6e0197b9fb75f428f309dc6f5
Rayh <railande.dasilva@gmail.com>
2023-09-27 21:26:16 -0300

    adicionando pasta de arquivos da IDE no gitignore

--- src/app/rayh/page.tsx
+++ src/app/rayh/page.tsx
@@ -1,11 +1,17 @@
+import Navbar from "@/component/rayh/Navbar";
+
 export default function RayhHome(){
 
     return(
-        <>
-            <button className="btn btn-primary">
-                testando
-            </button>
-            Portifólio rayh
-        </>
+        <main>
+            <div className="hero min-h-screen" style={{backgroundImage: 'url(https://images2.alphacoders.com/132/1329016.png)'}}>
+            <div className="hero-overlay bg-opacity-65"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">Rayh</h1>
+                    </div>
+                </div>
+            </div>
+        </main>
     )
 }
